8148714215ec187a0aa377c59a8beeb7b2028058
thanhnhan33.95@gmail.com <thanhnhan33.95@gmail.com>
2019-06-24 20:46:51 -0600

    Make changes in Makefile and sources.mk - add platform overide for SOURCES in source.mk, add dependency file generation

--- src/sources.mk
+++ src/sources.mk
@@ -10,11 +10,18 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
+ifeq ($(PLATFORM),HOST)
+SOURCES = ../src/main.c\
+	  ../src/memory.c
+endif
+
+ifeq ($(PLATFORM),MSP432)
 SOURCES = ../src/interrupts_msp432p401r_gcc.c\
 	  ../src/main.c\
 	  ../src/memory.c\
 	  ../src/startup_msp432p401r_gcc.c\
 	  ../src/system_msp432p401r.c
+endif
 
 # Add your include paths to this variable
 INCLUDES = -I../include/CMSIS\
